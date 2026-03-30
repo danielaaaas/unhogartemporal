85b5d822193adae5222a270f5426fcc85b15fc71
dandahdan <danieladiaz2898@gmail.com>
2026-03-30 16:32:35 -0400

    text test

--- java/main.js
+++ java/main.js
@@ -14,7 +14,7 @@
 const rooms = {
     'master-bedroom': {
         title: 'master bedroom',
-        text: 'to be added.'
+        text: 'current example, lorem ipsum dolor sit amet consectetur adipiscing elit. quisque faucibus ex sapien vitae pellentesque sem placerat. in id cursus mi pretium tellus duis convallis. tempus leo eu aenean sed diam urna tempor. pulvinar vivamus fringilla lacus nec metus bibendum egestas. iaculis massa nisl malesuada lacinia integer nunc posuere.'
     },
 
     'main-hallway': {
